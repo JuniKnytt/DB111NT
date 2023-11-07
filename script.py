1385cc23ebe6a35c444557ee3c639906ebc744f8
Juni Knytt <42306249+JuniKnytt@users.noreply.github.com>
2023-11-07 13:03:34 -0800

    added stocks

---
+++ script.py
@@ -0,0 +1,135 @@
+import sqlite3
+
+
+
+
+con = sqlite3.connect("data.db")
+cur = con.cursor()
+#1 SEEING USER COUNT
+print("\n#1 SEEING USER COUNT")
+res = cur.execute("SELECT count(*) AS cnt FROM Users u")
+print("User Count")
+print(res.fetchone())
+
+
+#2 NEW USER
+print("\n#2 NEW USER")
+data = [
+    (21, "user21", "user21@hotmail.com")
+]
+cur.executemany("INSERT INTO Users VALUES(?, ?, ?)", data)
+#con.commit() USE THESE TO EDIT DB
+
+#3 SEEING NEW USER
+print("\n#3 SEEING NEW USER")
+res = cur.execute("SELECT count(*) AS cnt FROM Users u")
+print("User Count")
+print(res.fetchone())
+
+#4 ADDING A NEW USER FAVORITE
+print("\n#4 ADDING A NEW USER FAVORITE")
+data = [
+    (13, 21, 1)
+]
+cur.executemany("INSERT INTO userFavResort VALUES(?, ?, ?)", data)
+#con.commit()
+
+#5 SHOWING USER FAVORITE
+print("\n#5 SHOWING USER FAVORITE")
+print("UserFavID", "UserID", "ResortID")
+for row in cur.execute("SELECT * FROM userFavResort ORDER BY userID"):
+    print(row)
+
+#6 READING USER1's REVIEW
+print("\n#6 READING USER1's REVIEW")
+print("Comment", "Score", "ResortID")
+for row in cur.execute("SELECT comment, rating, resortID FROM Review WHERE userID == 1 ORDER BY rating"):
+    print(row)
+
+#7 USER1 EDITING REVIEW OF 1
+print("\n#USER1 EDITING REVIEW OF Poor to Good Service")
+cur.execute("UPDATE Review SET rating = '7', comment= 'It got better' WHERE reviewID = 4")
+
+
+#8 READING AFTER EDIT REVIEW
+print("\n#8 READING AFTER EDIT REVIEW")
+print("Comment", "Score", "ResortID")
+for row in cur.execute("SELECT comment, rating, resortID FROM Review WHERE userID == 1 ORDER BY rating"):
+    print(row)
+
+#9 USER SEARCHES FOR RESORT WITH 10 RATING
+print("\n#9 USER SEARCHES FOR RESORTS WITH 10 RATING")
+print("ResortName", "Rating")
+for row in cur.execute("SELECT resortName, rating FROM Review r, SkiResort s WHERE s.resortID == r.resortID AND r.rating == 10"):
+    print(row)
+
+#10 USER SEARCHES FOR RESORT WITH RINKS
+print("\n#10 USER SEARCHES FOR RESORT WITH RINKS")
+print("ResortName")
+for row in cur.execute("SELECT resortName FROM SkiResort s, SkiResortActivities ra WHERE ra.activityID == 1 AND ra.resortID == s.resortID"):
+    print(row)
+
+#11 List Activity Names of Radical Snowway
+print("\n#11 List Activity Names of Radical Snowway")
+print("Activities")
+for row in cur.execute("select activityName FROM Activity a, SkiResortActivities sa, SkiResort r WHERE a.activityID == sa.activityID AND sa.resortID == r.resortID AND r.resortName == 'Radical Snoway'"):
+    print(row)
+
+#12 List Resort Favorite Count
+print("\n#12 List Resort Favorite Count")
+print("ResortName","NumberOfFaves")
+for row in cur.execute("SELECT resortName as Name, count(f.resortID) as cnt FROM SkiResort r, userFavResort f WHERE r.resortID == f.resortID Group by Name"):
+    print(row)
+
+#13 Resort that offers snow rink with the highest user favorite count
+print("\n#13 Resort that offers Snow rink with the highest user favorite count ")
+print("ResortName", "favCount")
+for row in cur.execute("Select Name, max(cnt) as cnt From (SELECT resortName as Name, count(f.resortID) as cnt FROM SkiResort r, userFavResort f WHERE r.resortID == f.resortID Group by Name) as l, SkiResortActivities sa, Activity a, SkiResort r WHERE l.Name == r.resortName AND r.resortID == sa.resortID AND sa.activityID == a.activityID AND a.activityName == 'snow rink'"):
+    print(row)
+
+#14 User4 Updates Email
+print("\n#14 User4 Updates Email")
+cur.execute("UPDATE Users SET userEmail = 'user4@califor@gmail.com' WHERE userID = 4")
+
+#15 Print userEmail and usernames that use Gmail
+print("\n#Print userEmail and usernames that use Gmail")
+print("username","userEmail")
+for row in cur.execute("Select username, userEmail From Users u WHERE u.userEmail LIKE '%gmail%'"):
+    print(row)
+
+#16 name of Resort with lowest adult booking price
+print("\n#16 name of Resort with lowest adult booking price")
+print("ResortName")
+for row in cur.execute("select resortName From (select resortName, min(costAdult) FROM Bookable b, SkiResort s where b.resortID == s.resortID)"):
+    print(row)
+
+#17 list resorts by elevation
+print("\n#17 list resorts by elevation")
+print("Name","Elevation")
+for row in cur.execute("select resortName as Name, resortElevation as elevation from skiresort s order by elevation"):
+    print(row)
+
+#18 show resorts that have pictures of their snow slopes and the picture url
+print("\n#18 show resorts that have pictures of their snow slopes and the picture url")
+print("Name","photoURL")
+for row in cur.execute("select resortName, photoURL From photo p, skiresort s where p.photoDescription LIKE '%Snow slopes%' AND p.resortID = s.resortID"):
+    print(row)
+
+#19 NEW USER REGISTERS
+print("\n#19 NEW USER REGISTERS")
+data = [
+    (22, "user22", "user22@gmail.com")
+]
+print("UserName", "UserEmail")
+for row in cur.execute("SELECT username, userEmail FROM Users ORDER BY userID"):
+    print(row)
+
+#20 NEW USER CREATES REVIEW AND Updated Board and sort best rated by top
+print("\n#20 NEW USER CREATES REVIEW and Show reviewboard")
+data = [
+    (6, 22, 'Amazing Slides', 9, '2023-12-12', 3)
+]
+cur.executemany("INSERT INTO Review VALUES(?, ?, ?, ?, ?, ?)", data)
+print("UserName", "Comment", "Rating", "ResortName")
+for row in cur.execute("select username, comment, rating, resortName from users u, review r, skiresort s where u.userID == r.userID and s.resortID == r.resortID order by rating DESC"):
+    print(row)
